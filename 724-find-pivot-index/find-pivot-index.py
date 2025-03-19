cdd3d0e48aacfc5dac87814e13e82d2103ef71be
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-19 20:49:06 +0530

    Time: 3 ms (87.34%) | Memory: 13.2 MB (80.72%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,16 +1,14 @@
 class Solution(object):
     def pivotIndex(self, n):
-
-        s=sum(n)
         l=0
-        r=s-n[0]
+        r=sum(n)-n[0]
 
         if l==r:
             return 0
+
         for i in range(1,len(n)):
             l+=n[i-1]
             r-=n[i]
-            print(l,r)
             if l==r:
                 return i
         return -1
