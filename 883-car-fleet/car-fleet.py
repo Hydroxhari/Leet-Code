a4bf9a5f2cc140138200caff065fbb11a5c83591
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-20 21:29:38 +0530

    Time: 222 ms (83.62%) | Memory: 31.5 MB (90.00%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,13 +1,8 @@
 class Solution(object):
     def carFleet(self, target, position, speed):
-        cars = sorted(zip(position, speed), reverse=True)
-        stack = []  # Stack to track fleet arrival times
-        
-        for pos, vel in cars:
-            time = float(target - pos) / vel  # Time to reach the target
-            
-            # If the stack is empty or the current car arrives later, it forms a new fleet
-            if not stack or time > stack[-1]:
+        stack = []
+        for pos, vel in sorted(zip(position, speed), reverse=True):
+            time = float(target - pos) / vel  
+            if not stack or time > stack[-1]:  
                 stack.append(time)
-        
-        return len(stack)  # Number of fleets
+        return len(stack)
