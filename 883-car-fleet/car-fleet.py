d07685591cc07e784ff14fef17eac82ef53d1b68
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-20 21:31:14 +0530

    Time: 127 ms (99.42%) | Memory: 33.8 MB (67.25%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,8 +1,20 @@
 class Solution(object):
     def carFleet(self, target, position, speed):
-        stack = []
+        cars_map={}
+        res=0
+        position_stack=[]
+        for i in range(len(position)):
+            cars_map[position[i]] = speed[i]
+        for car_pos in sorted(cars_map, reverse=True):
+            iterations=float(target-car_pos)/cars_map[car_pos]
+            if position_stack and position_stack[-1]>=iterations:
+                continue
+            else:
+                position_stack.append(iterations)
+        return len(position_stack)
+        '''stack = []
         for pos, vel in sorted(zip(position, speed), reverse=True):
             time = float(target - pos) / vel  
             if not stack or time > stack[-1]:  
                 stack.append(time)
-        return len(stack)
+        return len(stack)'''
