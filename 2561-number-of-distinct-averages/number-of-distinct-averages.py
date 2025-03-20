8ceaaa51651d6dfbbaf1e9b81f188d02fc9c683c
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-20 21:18:52 +0530

    Time: 3 ms (22.27%) | Memory: 12.6 MB (6.48%) - LeetSync

---
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def distinctAverages(self, n):
+        n.sort()
+        s=set()
+        l,r=0,len(n)-1
+        while l<r:
+            print(float(n[l]+n[r])/2)
+            s.add(float(n[l]+n[r])/2)
+            l+=1
+            r-=1
+        return len(s)
