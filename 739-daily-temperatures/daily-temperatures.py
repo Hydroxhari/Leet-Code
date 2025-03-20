25a5e3c8a782ab0be5759ed7ac00b01553327b41
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-20 15:14:31 +0530

    Time: 122 ms (98.97%) | Memory: 26.8 MB (68.52%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,11 +1,15 @@
 class Solution(object):
-    def dailyTemperatures(self, t):
-        s=[]
-        r=[0]*len(t)
-
-        for i in range(len(t)):
-            while s and t[s[-1]]<t[i]:
-                p=s.pop()
-                r[p]=i-p
-            s.append(i)
-        return r
+    def dailyTemperatures(self, temperatures):
+        n = len(temperatures)
+        res = [0] * n
+        hottest = temperatures[n-1]
+        for i in range(n-2, -1, -1):
+            if temperatures[i] >= hottest:
+                hottest = temperatures[i]
+                continue
+            days = 1
+            while temperatures[i+days] <= temperatures[i]:
+                days += res[i+days]
+            res[i] = days
+        return res
+    
