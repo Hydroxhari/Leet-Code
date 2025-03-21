706384e571829880c65ea72f05ccf61f626e4f97
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-21 12:50:02 +0530

    Time: 3 ms (97.77%) | Memory: 24.4 MB (49.54%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,5 +1,7 @@
 class Solution(object):
     def getCommon(self, n1, n2):
+        if n1[0] > n2[-1] or n1[-1] < n2[0]:
+            return -1
         l,r=0,0
         while l<len(n1) and r<len(n2):
             if n1[l]==n2[r]:
