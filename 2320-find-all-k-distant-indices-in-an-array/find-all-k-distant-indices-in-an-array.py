6c947a119bbebdcd38814da4fa2133d60f7b53ee
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-21 10:32:16 +0530

    Time: 63 ms (78.13%) | Memory: 12.8 MB (28.13%) - LeetSync

---
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def findKDistantIndices(self, n, t, k):
+
+        res=set()
+        l=0
+        for r in range(len(n)):
+            if n[r]==t:
+                while l<=r:
+                    if abs(r-l)<=k:
+                        res.add(l)
+                    l+=1
+        print(res)
+        l=len(n)-1
+        for r in range(len(n)-1,-1,-1):
+            if n[r]==t:
+                while l>=r:
+                    if abs(r-l)<=k:
+                        res.add(l)
+                    l-=1
+        print(res)
+        return sorted(res)
+
+
+        
