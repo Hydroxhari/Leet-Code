9115c337ab1e426f732c8015896e548170203f2d
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-21 11:50:57 +0530

    Time: 4 ms (55.74%) | Memory: 12.4 MB (80.42%) - LeetSync

---
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minimumAverage(self, n):
+
+        n.sort()
+        l,r=0,len(n)-1
+        m=float('inf')
+        while l<r:
+            m=min(m,float(n[l]+n[r])/2)
+            l+=1
+            r-=1
+        return m
