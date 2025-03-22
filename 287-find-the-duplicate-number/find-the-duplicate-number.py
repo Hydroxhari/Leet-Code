c634974bbef845367f9abb8c5bfc9616a334158f
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-22 11:16:47 +0530

    Time: 31 ms (74.94%) | Memory: 27.3 MB (11.50%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,18 +1,7 @@
 class Solution(object):
     def findDuplicate(self, nums):
-        slow = fast = nums[0]
-        
-        while True:
-            slow = nums[slow]
-            fast = nums[nums[fast]]
-            if slow == fast:
-                break
-        
-        # Step 2: Find the duplicate entry point
-        slow = nums[0]
-        while slow != fast:
-            slow = nums[slow]
-            fast = nums[fast]
-        
-        return slow
-
+        seen=set()
+        for i in nums:
+            if i in seen:
+                return i
+            seen.add(i)
