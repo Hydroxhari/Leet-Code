1c9f3d1760326a8d79236ce4fd7f6fc0b75a883d
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-23 12:12:16 +0530

    Time: 28 ms (60.29%) | Memory: 19.6 MB (39.71%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
@@ -1,45 +1,30 @@
-from collections import defaultdict
 import heapq
-
 class Solution(object):
-    def countPaths(self, n, r):
-        # Step 1: Build the graph
-        graph = defaultdict(list)
-        for u, v, t in r:
-            graph[u].append((v, t))
-            graph[v].append((u, t))
-        
-        # Step 2: Dijkstra's Initialization
-        mod = 10**9 + 7
-        min_time = [float('inf')] * n   # Stores the shortest time to reach each node
-        path_count = [0] * n            # Stores the number of ways to reach each node
-        min_time[0] = 0                 # Start from node 0 with time 0
-        path_count[0] = 1               # There's one way to start from node 0
-
-        # Priority queue to store (time, node)
-        pq = [(0, 0)]  # (time to reach the node, node)
-
-        # Step 3: Dijkstra's Algorithm
+    def countPaths(self, n, roads):
+        """
+        :type n: int
+        :type roads: List[List[int]]
+        :rtype: int
+        """
+        adj=defaultdict(list)
+        res=[float('inf')]*n
+        ways=[0]*n
+        for u,v,time in roads:
+            adj[u].append((v,time))
+            adj[v].append((u,time))
+        pq=[]
+        res[0]=0
+        ways[0]=1
+        heapq.heappush(pq,(0,0))
+        count=0
         while pq:
-            current_time, node = heapq.heappop(pq)
+            d,node=heapq.heappop(pq)
+            for v,dist in adj[node]:
+                if d+dist<res[v]:
+                    res[v]=d+dist
+                    ways[v]=ways[node]
+                    heapq.heappush(pq,(d+dist,v))
+                elif d+dist==res[v]: ways[v]+=ways[node]
+        return ways[n-1]%(10**9+7)
 
-            # If the current time is greater than the recorded shortest time, skip it
-            if current_time > min_time[node]:
-                continue
-            
-            # Explore all neighbors
-            for neighbor, travel_time in graph[node]:
-                new_time = current_time + travel_time
-
-                # Found a **shorter path** to the neighbor
-                if new_time < min_time[neighbor]:
-                    min_time[neighbor] = new_time
-                    path_count[neighbor] = path_count[node]
-                    heapq.heappush(pq, (new_time, neighbor))
-
-                # Found **another path** with the **same shortest time**
-                elif new_time == min_time[neighbor]:
-                    path_count[neighbor] = (path_count[neighbor] + path_count[node]) % mod
-
-        # Step 4: Return the number of shortest paths to node n-1
-        return path_count[n - 1]
+        
