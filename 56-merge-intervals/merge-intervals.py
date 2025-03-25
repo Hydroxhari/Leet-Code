63c53cff0e976fb831bfb87b2a5fe1492abc86a1
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-25 09:39:59 +0530

    Time: 12 ms (42.28%) | Memory: 15.9 MB (84.13%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, n):
+
+        n.sort()
+        r=[]
+        for i in n:
+            if r and r[-1][1]>=i[0]:
+                r[-1][1]=max(r[-1][1],i[1])
+            else:
+                r.append(i)
+
+        return r           
+
