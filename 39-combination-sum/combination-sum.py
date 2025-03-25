87a9fa6eeab481572f876047c1fa6810826edbb1
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-25 18:40:26 +0530

    Time: 7 ms (95.08%) | Memory: 12.5 MB (52.22%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,20 +1,20 @@
 class Solution(object):
     def combinationSum(self, c, k):
-        s=set()
+        s=[]
         c.sort()
 
-        def bt(l,t):
+        def bt(st,l,t):
             if t==0:
-                s.add(tuple(sorted(l[:])))
+                s.append(l[:])
                 return
             
-            for i in range(len(c)):
-                if t-c[i]>=0:
-                    l.append(c[i])
-                    bt(l,t-c[i])
-                    l.pop()
-                else:
-                    return
-        bt([],k)
-        return list(list(i) for i in s)
+            for i in range(st,len(c)):
+                if t<c[i]:
+                    break
+                l.append(c[i])
+                bt(i,l,t-c[i])
+                l.pop()
+
+        bt(0,[],k)
+        return s
 
