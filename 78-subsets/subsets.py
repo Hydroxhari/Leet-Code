8ecdc216fec81473d06c6613113a385dc68482af
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-03-25 18:58:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (19.44%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -6,7 +6,9 @@ class Solution(object):
         def bt(s,l):
             r.append(l[:])
             for i in range(s,len(n)):
-                bt(i+1,l+[n[i]])
+                l.append(n[i])
+                bt(i+1,l)
+                l.pop()
         
         bt(0,[])
         return r
