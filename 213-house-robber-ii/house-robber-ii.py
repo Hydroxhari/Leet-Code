fc2ce1080831f794ba459db1038b0edc5d66ae9c
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-04-21 18:02:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (15.58%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def rob(self, n):
+        l=len(n)
+        if l==0:
+            return 0
+        if l==1:
+            return n[0]
+
+        def r(n):
+            l=len(n)
+            if l==0:
+                return 0
+            if l==1:
+                return n[0]
+                
+            dp=[0]*l
+            dp[0]=n[0]
+            dp[1]=max(n[0],n[1])
+
+            for i in range(2,l):
+                dp[i]=max(dp[i-1],n[i]+dp[i-2])
+
+            return dp[l-1]
+            
+        return max(r(n[1:]),r(n[:-1]))
