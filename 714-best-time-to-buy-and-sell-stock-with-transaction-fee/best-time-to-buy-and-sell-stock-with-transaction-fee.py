f07f1e6a7caa87d23431d809c3d414df2bbdde41
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-04-24 16:36:57 +0530

    Time: 99 ms (57.09%) | Memory: 16.5 MB (83.46%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,15 +1,12 @@
 class Solution(object):
     def maxProfit(self, p, f):
 
-        l=len(p)
-        b = [0]*l
-        s = [0]*l
+        b = -p[0]
+        s=0
 
-        b[0] = -p[0]
-
-        for i in range(1,l):
-            b[i] = max(b[i-1],s[i-1]- p[i])
-            s[i] = max(s[i-1],b[i-1]+p[i]-f)
+        for i in range(1,len(p)):
+            b= max(b,s- p[i])
+            s= max(s,b+p[i]-f)
         
-        return s[-1]
+        return s
         
