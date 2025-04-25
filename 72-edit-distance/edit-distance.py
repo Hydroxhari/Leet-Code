44af60780c37a0fdd66ce336dc88a0870c397214
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-04-25 18:32:41 +0530

    Time: 40 ms (85.07%) | Memory: 20.1 MB (68.61%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,23 +1,28 @@
+from functools import lru_cache
+
 class Solution:
     def minDistance(self, w: str, t: str) -> int:
 
+        l, k = len(w), len(t)
 
-        l,k=len(w),len(t)
-
-        @lru_cache(None)
-        def dfs(i,j,c):
-            if j==k:
-                return c + (l-i)
-            if i==l:
-                return c + (k-j) 
+        @lru_cache(None)  # Memoization cache
+        def dfs(i, j):
+            # Base cases
+            if i == l:  # If we have exhausted all characters in w
+                return k - j  # We need to insert the remaining characters of t
+            if j == k:  # If we have exhausted all characters in t
+                return l - i  # We need to delete the remaining characters of w
             
-            if w[i]!=t[j]:
-                c= min( dfs(i+1,j,c+1),
-                        dfs(i,j+1,c+1),
-                        dfs(i+1,j+1,c+1) )
-            else:
-                return dfs(i+1,j+1,c)
+            # If characters match, no operation needed, move to the next characters
+            if w[i] == t[j]:
+                return dfs(i + 1, j + 1)
 
-            return c
+            # Otherwise, calculate the minimum number of operations (insert, delete, replace)
+            delete = dfs(i + 1, j)  # Deleting a character from w
+            insert = dfs(i, j + 1)  # Inserting a character into w (i.e., matching it to t)
+            replace = dfs(i + 1, j + 1)  # Replacing a character in w with the one in t
 
-        return dfs(0,0,0)
+            return 1 + min(delete, insert, replace)  # Add 1 for the current operation
+
+        # Start from the beginning of both strings
+        return dfs(0, 0)
