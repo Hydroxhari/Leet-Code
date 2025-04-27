61bd8d3f976bd827125ebf722940c9224dd5cfd8
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-04-27 10:03:55 +0530

    Time: 211 ms (9.38%) | Memory: 20.7 MB (6.25%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
@@ -1,22 +1,24 @@
-from typing import List
+class Solution(object):
+    def countSubarrays(self, n, l, m):
 
-class Solution:
-    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
-        last_min = -1  # the last index where nums[i] == minK
-        last_max = -1  # the last index where nums[i] == maxK
-        last_invalid = -1  # the last index where nums[i] < minK or nums[i] > maxK
-        res = 0  # the final result (number of valid subarrays)
+        a=-1
+        b=-1
+        d=-1
+        c=0
+        j=0
 
-        for i, num in enumerate(nums):
-            if num < minK or num > maxK:
-                last_invalid = i  # If num is invalid, reset window
-            if num == minK:
-                last_min = i  # update the last place we found minK
-            if num == maxK:
-                last_max = i  # update the last place we found maxK
+        for i in range(len(n)):
+            if l>n[i] or n[i]>m:
+                d=i
+            
+            if n[i]==l:
+                a=i
+            
+            if n[i]==m:
+                b=i
+            
+            c+= max(0,min(a,b)-d)
+        
+        return c
 
-            # Now, if both minK and maxK have appeared **after** last_invalid,
-            # We can form (min(last_min, last_max) - last_invalid) subarrays ending at i
-            res += max(0, min(last_min, last_max) - last_invalid)
 
-        return res
