a7b17233bf199f0c8330eb92dc92eed4f9d0c2a9
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-06-11 15:43:41 +0530

    Time: 43 ms (32.33%) | Memory: 13.2 MB (80.52%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -2,13 +2,8 @@ class Solution(object):
     def canJump(self, n):
 
         s=n[0]
-        if len(n)==1:
-            return True
-        if s==0:
-            return False
-
-        for i in range(1,len(n)-1):
-            s=max(s-1,n[i])
-            if s==0:
+        for i in range(1,len(n)):
+            if s<i:
                 return False
+            s=max(s,i+n[i])
         return True
