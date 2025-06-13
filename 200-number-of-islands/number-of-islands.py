ea4b35a2b41996f72334834adca6c907c2a0aff3
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-06-13 12:30:28 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (23.92%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,36 +1,33 @@
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 
+
 class Solution(object):
     def numIslands(self, grid):
         if not grid:
             return 0
         
-        m, n = len(grid), len(grid[0])
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        rows, cols = len(grid), len(grid[0])
         visited = set()
-        island_count = 0
+        directions = [(-1,0), (1,0), (0,-1), (0,1)]
+        count = 0
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1' and (i,j) not in visited:
+                    count += 1
+                    queue = deque([(i,j)])
+                    visited.add((i,j))
+                    
+                    while queue:
+                        r, c = queue.popleft()
+                        for dr, dc in directions:
+                            nr, nc = r + dr, c + dc
+                            if (0 <= nr < rows and 0 <= nc < cols and 
+                                grid[nr][nc] == '1' and (nr, nc) not in visited):
+                                queue.append((nr, nc))
+                                visited.add((nr, nc))
+        
+        return count
 
-        def bfs(row, col):
-            queue = deque([(row, col)])
-            visited.add((row, col))
 
-            while queue:
-                r, c = queue.popleft()
 
-                # Explore neighbors
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-
-                    if (0 <= nr < m and 0 <= nc < n and
-                        (nr, nc) not in visited and grid[nr][nc] == '1'):
-                        queue.append((nr, nc))
-                        visited.add((nr, nc))
-
-        # Iterate through the entire grid
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1' and (i, j) not in visited:
-                    island_count += 1  # Found a new island
-                    bfs(i, j)          # Explore the entire island
-
-        return island_count
