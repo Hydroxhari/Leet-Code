a2f6c7c90ab66977379e6e80655f7eb2b009dd80
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-06-21 17:25:57 +0530

    Time: 83 ms (94.57%) | Memory: 17.9 MB (59.08%) - LeetSync

--- 3815-sum-of-largest-prime-substrings/sum-of-largest-prime-substrings.py
+++ 3815-sum-of-largest-prime-substrings/sum-of-largest-prime-substrings.py
@@ -1,27 +1,30 @@
+from functools import lru_cache
+
 class Solution(object):
     def sumOfLargestPrimes(self, s):
-
-        def ip(n):
-            if n<=1:
+        @lru_cache(None)
+        def is_prime(n):
+            if n <= 1:
                 return False
-            if n<=3:
+            if n <= 3:
                 return True
-            if n%2==0 or n%3==0:
+            if n % 2 == 0 or n % 3 == 0:
                 return False
-            for i in range(5,int(n**0.5)+1,6):
-                if n%i==0 or n%(i+2)==0:
+            for i in range(5, int(n**0.5) + 1, 6):
+                if n % i == 0 or n % (i + 2) == 0:
                     return False
-            return True        
-        
-        u=set()
+            return True
 
-        for i in range(len(s)):
-            for j in range(i,len(s)):
-                n=int(s[i:j+1])
-                if ip(n):
-                    print(n)
-                    u.add(n)
-        
-        l=list(u)
-        return sum(sorted(l)[-3:])
-        
+        u = set()
+        n = len(s)
+
+        for i in range(n):
+            if s[i] == '0':
+                continue  # Skip substrings starting with 0
+            for j in range(i, n):  # Limit to max 9-digit substrings
+                num = int(s[i:j + 1])
+                if is_prime(num):
+                    u.add(num)
+
+        top_primes = sorted(u, reverse=True)
+        return sum(top_primes[:3])
