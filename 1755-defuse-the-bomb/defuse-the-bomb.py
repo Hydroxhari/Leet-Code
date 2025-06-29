32fd1373d4c1744d65d7fad62032b2b03a80d6bc
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-06-29 21:25:17 +0530

    Time: 1 ms (76.08%) | Memory: 12.4 MB (84.39%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.py
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def decrypt(self, c, k):
 
-        a=[]
+        '''a=[]
         l=len(c)
         if k==0:
             return [0]*l
@@ -14,4 +14,20 @@ class Solution(object):
                 for j in range(1,k+1):
                     s+=c[(i+j)%l]
             a.append(s)
+        return a'''
+
+        l=len(c)
+
+        if k==0:
+            return [0]*l
+        
+        n=c+c+c
+
+        a=[]
+
+        for i in range(l,l+l):
+            if k<0:
+                a.append(sum(n[i+k:i]))
+            if k>0:
+                a.append(sum(n[i+1:i+k+1]))
         return a
