f39deb7bb376cfa3e2063dfed691fd95f16a1fad
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-07-02 19:50:36 +0530

    Time: 133 ms (22.54%) | Memory: 19.1 MB (71.49%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,10 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
-
 class Solution(object):
     def maxProfit(self, p):
-        a=p[0]
-        m=0
-        for i in p[1:]:
-            a=min(a,i)
-            m=max(m,i-a)
-        return m
+
+        b=float('inf')
+        s=float('-inf')
+
+        for i in p:
+            b=min(b,i)
+            s=max(s,i-b)
+        return s
