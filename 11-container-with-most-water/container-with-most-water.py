37b8e69b758c3294e85f937800668a90fe0faea9
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-07-02 17:49:32 +0530

    Time: 107 ms (73.30%) | Memory: 20.7 MB (54.11%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,19 +1,15 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        max_water = 0
-        i = 0
-        j = len(height)-1
-        while i<j:
-            water = min(height[i],height[j])* (j-i)
-            max_water = max(water,max_water)
+    def maxArea(self, ht):
+        
+        m=0
+        i,j=0,len(ht)-1
 
-            if height[i] > height[j]:
+        while i<j:
+            s=(j-i)*min(ht[i],ht[j])
+            m=max(m,s)
+
+            if ht[i]>ht[j]:
                 j-=1
             else:
                 i+=1
-        return max_water
+        return m
