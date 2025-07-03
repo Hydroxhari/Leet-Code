8c3ece70cee38e1c266cba43860d7d746111ecc4
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-07-03 13:50:57 +0530

    Time: 17 ms (42.18%) | Memory: 19.9 MB (74.80%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,16 +1,17 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
-
-class Solution(object):
-    def minPathSum(self, g):
-        m,n=len(g),len(g[0])
-        dp=g
+class Solution:
+    def minPathSum(self, dp: List[List[int]]) -> int:
         
+        m,n=len(dp),len(dp[0])
+
         for i in range(1,m):
             dp[i][0]+=dp[i-1][0]
         for j in range(1,n):
-            dp[0][j]+=dp[0][j-1]        
+            dp[0][j]+=dp[0][j-1]
+
         for i in range(1,m):
             for j in range(1,n):
-                dp[i][j]+=min(dp[i-1][j],dp[i][j-1])
-        
-        return dp[m-1][n-1]
+                dp[i][j]=min(dp[i][j]+dp[i-1][j],dp[i][j]+dp[i][j-1])
+        return dp[-1][-1]
+
+
+
