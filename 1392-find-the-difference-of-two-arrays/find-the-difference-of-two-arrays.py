1c0dba1f76b91a0beab276dc8c6864cdacdde2b8
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-07-19 19:20:43 +0530

    Time: 12 ms (42.29%) | Memory: 12.6 MB (88.68%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -1,3 +1,3 @@
 class Solution(object):
     def findDifference(self, n, m):
-        return [list(set(n)-(set(n)&set(m))) , list(set(m)-(set(n)&set(m)))]
+        return [list(set(n)-set(m)),list(set(m)-set(n))]
