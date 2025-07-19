8360b59d58a9f9a519de2b97c535dcef5948fa33
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-07-19 17:32:54 +0530

    Time: 1395 ms (18.18%) | Memory: 53.1 MB (7.05%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,11 +1,19 @@
 class Solution(object):
     def coinChange(self, c, a):
 
-        dp=[float('inf')]*(a+1)
-        dp[0]=0
-        for i in range(1,a+1):
-            for j in c:
-                if i-j>=0:
-                    dp[i]=min(dp[i],1+dp[i-j])
-        return dp[-1] if dp[-1]!=float('inf') else -1
 
+        memo={}
+        def dp(a):
+            if a==0:
+                return 0
+            if a in memo:
+                return memo[a]
+            m=float('inf')
+            for i in c:
+                if a-i>=0:
+                    m=min(m,1+dp(a-i))
+
+            memo[a]=m
+            return m
+
+        return dp(a) if dp(a)!=float('inf') else -1
