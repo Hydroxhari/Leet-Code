55227ca552206024ec50e67c04a2f579753abbd0
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-07-20 11:34:00 +0530

    Time: 139 ms (77.95%) | Memory: 12.6 MB (99.26%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,14 +1,9 @@
-class Solution:
-    def change(self, a: int, c: List[int]) -> int:
+class Solution(object):
+    def change(self, a, c):
 
-        c.sort()
-        @lru_cache(None)
-        def dp(a,s):
-            if a==0:
-                return 1 
-            if a<0 or s==len(c):
-                return 0
-            
-            return dp(a-c[s],s)+dp(a,s+1)
-
-        return dp(a,0)
+        dp=[0]*(a+1)
+        dp[0]=1
+        for i in c:
+            for j in range(i,a+1):
+                dp[j]+=dp[j-i]
+        return dp[-1]
