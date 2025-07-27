6d432cb2a050e3090455c9bf8dfe37b86a4c36df
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-07-27 15:53:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (18.18%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
@@ -5,18 +5,11 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        filtered = [nums[0]]
-
-        # Remove consecutive duplicates
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
-                filtered.append(nums[i])
-
-        # Check for hills or valleys
-        for i in range(1, len(filtered) - 1):
-            if filtered[i] > filtered[i - 1] and filtered[i] > filtered[i + 1]:
-                count += 1  # Hill
-            elif filtered[i] < filtered[i - 1] and filtered[i] < filtered[i + 1]:
-                count += 1  # Valley
-
+        for i in range(1, len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i] = nums[i - 1]
+            if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
+                count += 1
+            if nums[i] < nums[i - 1] and nums[i] < nums[i + 1]:
+                count += 1
         return count
