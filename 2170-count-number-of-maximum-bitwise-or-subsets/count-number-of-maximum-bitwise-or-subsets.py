ca14fcae8edcd312b20719b8f65d9e90cd14cd57
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-07-28 19:05:21 +0530

    Time: 8 ms (96.08%) | Memory: 12.6 MB (43.14%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -1,12 +1,39 @@
+import heapq
 class Solution(object):
     def countMaxOrSubsets(self, nums):
-        dp = [0] * (1 << 17)
-        dp[0] = 1
-        max_or = 0
-
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # problem formulation:
+        #   max_or -> maximum value of bitwise or subset 
+        #   return all subsets with max_or 
+        # rule: a or b = max -> anything with 
+        value_dict = {}
+        cur_max = 0
         for num in nums:
-            for i in range(max_or, -1, -1):
-                dp[i | num] += dp[i]
-            max_or |= num
+            
+            cur_dict = value_dict.copy()
+            # print(cur_dict)
+            if num > cur_max:
+                cur_max = num
+            if num not in value_dict:
+                value_dict[num]=1
+            else: value_dict[num]+=1
+            for value in cur_dict:
+                bitwise_or_value = value|num
+                if bitwise_or_value > cur_max:
+                    cur_max = bitwise_or_value
+                if bitwise_or_value not in value_dict:
+                    value_dict[bitwise_or_value] = cur_dict[value]
+                else:
+                    value_dict[bitwise_or_value] += cur_dict[value]
+        return value_dict[cur_max]
+        
+        # 1: 3
+        # 2 2, 2, 2
+        
+        
+        
 
-        return dp[max_or] 
+        
