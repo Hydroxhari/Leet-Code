8a4a78ef9bb5f958f2a60427bfc7000961663ac0
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-01 18:26:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (56.57%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def generate(self, n):
-
-        l=[[1]*i for i in range(1,n+1)]
-
-        for i in range(2,n):
+        l=[]
+        for i in range(n):
+            r=[1]*(i+1)
             for j in range(1,i):
-                l[i][j]=l[i-1][j-1]+l[i-1][j]
+                r[j]=l[i-1][j-1]+l[i-1][j]
+            l.append(r)
+        return(l)
+
+
         
-        return l
         
