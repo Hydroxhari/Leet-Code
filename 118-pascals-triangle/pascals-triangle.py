881053cb6943fc339b92714b19ed08e80637be57
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-01 18:21:19 +0530

    Time: 4 ms (11.37%) | Memory: 12.6 MB (21.66%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def generate(self, n):
+
+        l=[[1]*i for i in range(1,n+1)]
+
+        for i in range(2,n):
+            for j in range(1,i):
+                l[i][j]=l[i-1][j-1]+l[i-1][j]
+        
+        return l
+        
