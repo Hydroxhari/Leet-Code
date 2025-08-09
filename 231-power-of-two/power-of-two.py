95141fab7f64a5ae47ec929007c5f555593c9d9a
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-09 10:46:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (9.90%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -1,17 +1,4 @@
 class Solution(object):
     def isPowerOfTwo(self, n):
-
-        if n<=0:
-            return False
-        if n==1:
-            return True
-        if n%2!=0:
-            return False
+        return n>0 and (n&(n-1))==0
         
-        while n>2:
-            n//=2
-            if n%2!=0:
-                return False
-        return True
-
-            
