f9cb594086c25e35e24448b49d2b5578e27f6d02
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-14 20:05:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (88.37%) - LeetSync

---
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def largestGoodInteger(self, n):
+
+        for i in range(9,-1,-1):
+            if str(i)*3 in n:
+                return str(i)*3
+        return ''
+
+
+        
