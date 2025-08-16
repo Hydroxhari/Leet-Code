13189c8d6fd4e58d44effd4b8ef6bd4f055dfe79
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-16 14:20:54 +0530

    Time: 23 ms (46.55%) | Memory: 14.8 MB (50.21%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -4,13 +4,11 @@ class Solution(object):
         r=[1]*len(l)
 
         for i in range(1,len(l)):
-            if l[i]>l[i-1] and r[i]<=r[i-1]:
+            if l[i]>l[i-1]:
                 r[i]=r[i-1]+1
-        
-        print(r)
 
         for i in range(len(l)-2,-1,-1):
-            if l[i]>l[i+1] and r[i]<=r[i+1]:
-                r[i]=r[i+1]+1
+            if l[i]>l[i+1]:
+                r[i]=max(r[i],r[i+1]+1)
         
         return sum(r)
