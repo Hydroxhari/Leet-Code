5a44dd235e7c4d97c57fb236aa9932ff653346ad
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-16 14:16:44 +0530

    Time: 231 ms (5.66%) | Memory: 14.7 MB (74.73%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,13 +1,16 @@
 class Solution(object):
-    def candy(self, ratings):
-        n = len(ratings)
-        cnt = 0
-        candies = [1] * n
-        for i in range(1, n):
-            if ratings[i] > ratings[i - 1]:
-                candies[i] = candies[i - 1] + 1
-        for i in range(n - 1, 0, -1):
-            if ratings[i - 1] > ratings[i]:
-                candies[i - 1] = max(candies[i] + 1, candies[i - 1])
-            cnt += candies[i - 1]
-        return cnt + candies[n - 1]
+    def candy(self, l):
+
+        r=[1]*len(l)
+
+        for i in range(1,len(l)):
+            if l[i]>l[i-1] and r[i]<=r[i-1]:
+                r[i]=r[i-1]+1
+        
+        print(r)
+
+        for i in range(len(l)-2,-1,-1):
+            if l[i]>l[i+1] and r[i]<=r[i+1]:
+                r[i]=r[i+1]+1
+        
+        return sum(r)
