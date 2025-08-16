7a87efe9c5597ed3a4975871702c8e1be368b697
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-16 16:56:05 +0530

    Time: 3 ms (54.70%) | Memory: 12.7 MB (50.68%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,21 +1,18 @@
 class Solution(object):
     def permute(self, n):
-        
-        r=[]
-        u=[False]*len(n)
 
-        def bt(l):
-            if len(l)==len(n):
-                r.append(l[:])
+        ls=[]
+        
+        def bt(l,v):
+            if len(v)==len(n):
+                ls.append(l)
                 return
             
-            for i in range(len(n)):
-                if not u[i]:
-                    u[i]=True
-                    l.append(n[i])
-                    bt(l)
-                    l.pop()
-                    u[i]=False
-        bt([])
-        return r
-        
+            for i in n:
+                if i not in v:
+                    v.add(i)
+                    bt(l+[i],v)
+                    v.remove(i)
+            
+        bt([],set())
+        return ls
