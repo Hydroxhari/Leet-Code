e4a9b305c3e066d3ae873e54bb947ca81022ce67
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-16 14:05:34 +0530

    Time: 7 ms (82.52%) | Memory: 13.3 MB (34.40%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,15 +1,16 @@
 class Solution(object):
     def asteroidCollision(self, a):
 
-        l=[]
+        s=[]
         for i in a:
             if i<0:
-                while l and l[-1]>0 and l[-1]<-i :
-                    l.pop()
-                if l and l[-1]==-i:
-                    l.pop()
-                elif not l or l[-1]<0:
-                    l.append(i)
+                while s and s[-1]>0 and s[-1]<-i:
+                    s.pop()
+                if s and s[-1]==-i:
+                    s.pop()
+                    continue
+                if not s or s[-1]<0:
+                    s.append(i)
             else:
-                l.append(i)
-        return l
+                s.append(i)
+        return s
