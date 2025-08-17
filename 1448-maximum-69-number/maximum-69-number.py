81c5f93ca70a8f100b1479c881dad32d5a3a1890
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-17 14:31:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (8.28%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.py
+++ 1448-maximum-69-number/maximum-69-number.py
@@ -1,12 +1,3 @@
 class Solution(object):
-    def maximum69Number (self, n):
-        s=str(n)
-        nn=0
-        a=1
-        for i in s:
-            cn=int(i)
-            if a and i=='6':
-                cn=9
-                a-=1
-            nn=nn*10+cn
-        return nn
+    def maximum69Number (self, num):
+        return int(str(num).replace('6', '9', 1))
