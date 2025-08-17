28f4d79d4b08159114ce955c98603a325d260baf
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-17 13:22:53 +0530

    Time: 47 ms (58.13%) | Memory: 15.6 MB (88.55%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -1,24 +1,16 @@
 class Solution(object):
     def equalPairs(self, g):
 
-        r=defaultdict(int)
-        m,n=len(g),len(g[0])
-
-        for i in range(m):
-            nu=''
-            for j in range(n):
-                nu+=str(g[i][j])+'!'
-            r[nu]+=1
-
-        c=defaultdict(int)
-        for i in range(n):
-            nu=''
-            for j in range(m):
-                nu+=str(g[j][i])+'!'
-            c[nu]+=1
+        d=defaultdict(int)
+        for i in g:
+            d[tuple(i)]+=1
         
-        a=0
-        for i,j in r.items():
-            if i in c:
-                a+=j*c[i]
-        return a
+        c=0
+        for i in range(len(g)):
+            l=[]
+            for j in range(len(g)):
+                l.append(g[j][i])
+            c+=1*d[tuple(l)]
+        return c
+
+        
