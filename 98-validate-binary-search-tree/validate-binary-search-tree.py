f9b92b2492d16342ea3d70484a7471c3ca6e60da
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-17 13:17:12 +0530

    Time: 3 ms (68.87%) | Memory: 17 MB (76.60%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -1,29 +1,12 @@
-from collections import deque
-
 class Solution(object):
-    def isValidBST(self, root):
-        if not root:
-            return True
+    def isValidBST(self, r):
         
-        # Use deque for BFS (node, min_value, max_value)
-        q = deque([(root, float('-inf'), float('inf'))])
-        
-        while q:
-            node, low, high = q.popleft()
-            
-            if not node:
-                continue
-            
-            # Check if the current node violates the BST property
-            if not (low < node.val < high):
+        def bst(v,lf,ri):
+            if not v:
+                return True
+            if not (lf<v.val<ri):
                 return False
             
-            # Left child: max value should be the current node's value
-            if node.left:
-                q.append((node.left, low, node.val))
-            
-            # Right child: min value should be the current node's value
-            if node.right:
-                q.append((node.right, node.val, high))
-        
-        return True
+            return bst(v.left,lf,v.val) and bst(v.right,v.val,ri)
+
+        return bst(r,float('-inf'),float('inf'))
