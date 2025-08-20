3dcd44bb4177ee1a94f2b41f8328d32dbc98b085
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-20 12:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (95.88%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,5 +1,25 @@
-class Solution(object):
-    def search(self, nums, target):
-        s=set(nums)
-        return target in s
-        
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        low = 0
+        high = len(nums)-1
+        while low <= high:
+            mid = (low+high)//2
+            if nums[mid] == target:
+                return True
+            # handling all duplicate condition
+            if nums[low] == nums[mid] == nums[high]:
+                low +=1
+                high-=1
+            # when the left side is sorted
+            elif nums[mid] >= nums[low]:
+                if target >= nums[low] and target < nums[mid]:
+                    high = mid-1
+                else:
+                    low = mid+1
+            # when the right side is sorted
+            else:
+                if target <= nums[high] and target > nums[mid]:
+                    low = mid+1
+                else:
+                    high = mid-1
+        return False
