aebf4064789371553f3a4902b62b76a8693be5db
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-24 13:34:30 +0530

    Time: 807 ms (65.26%) | Memory: 12.7 MB (96.06%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -4,23 +4,13 @@ class Solution(object):
         s=sum(n)
         if s%2!=0:
             return False
-        t=s//2
-
-        memo={}
-
-        def dp(r,i):
-            if (r,i) in memo:
-                return memo[(r,i)]
-            if r==0:
-                memo[(r,i)]=True
-                return True
-            if r<0 or i==len(n):
-                memo[(r,i)]=False
-                return False
             
+        r=s//2
 
+        dp=[1]+[0]*(r)
 
-            memo[(r,i)]=dp(r-n[i],i+1) or dp(r,i+1)
-            return memo[(r,i)]
-        
-        return dp(t,0)
+        for i in n:
+            for j in range(r,i-1,-1):
+                dp[j]=dp[j] or dp[j-i]
+                
+        return dp[-1]==1
