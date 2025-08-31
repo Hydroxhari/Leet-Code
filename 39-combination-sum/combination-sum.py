4e2ee76e24a6837a8072028ffee87c90bf47a0de
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-08-31 10:13:33 +0530

    Time: 34 ms (12.50%) | Memory: 12.5 MB (80.15%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -3,15 +3,15 @@ class Solution(object):
 
         s=set()
 
-        def bt(t,al):
+        def bt(i,t,al):
             if t==0:
                 nt=tuple(sorted(al))
                 s.add(nt)
-            
-            for i in l:
-                if t-i>=0:
-                    bt(t-i,al+[i])
-        
-        bt(t,[])
+            if i>=len(l) or t<0:
+                return 0
+            take = bt(i,t-l[i],al+[l[i]])
+            dtake= bt(i+1,t,al)
+
+        bt(0,t,[])
 
         return list(s)
