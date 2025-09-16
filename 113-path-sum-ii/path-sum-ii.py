0c92c03b23c34bfde66e5f9b38f77d4133f34dee
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-09-16 23:11:30 +0530

    Time: 4 ms (54.32%) | Memory: 19.1 MB (7.61%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def pathSum(self, r, t):
+        a=[]
+        l=[]
+        def ps(r,t,l):
+            if not r:
+                return 
+            if not r.left and not r.right and r.val==t:
+                l=l+[r.val]
+                a.append(l)
+                return 
+            t=t-r.val
+            ps(r.left,t,l+[r.val])
+            ps(r.right,t,l+[r.val])
+        ps(r,t,l)
+        return a
+            
