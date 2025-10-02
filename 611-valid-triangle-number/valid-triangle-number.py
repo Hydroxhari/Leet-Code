da7449c1bf17983cd7913ed8794d8eb9ebdeaf62
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-10-03 01:32:11 +0530

    Time: 543 ms (42.68%) | Memory: 12.6 MB (44.54%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.py
+++ 611-valid-triangle-number/valid-triangle-number.py
@@ -1,15 +1,23 @@
-class Solution:
-    def triangleNumber(self, nums: List[int]) -> int:
-        nums.sort()
+from itertools import combinations
+class Solution(object):
+    def triangleNumber(self, nums):
+        nums.sort()  # Sorting helps in efficient two-pointer checking
         n = len(nums)
         count = 0
-
-        for i in range(n - 1, -1, -1):
-            left, right = 0, i - 1
-            while left < right:
-                if nums[left] + nums[right] > nums[i]:
-                    count += right - left
-                    right -= 1
+        
+        # Fix the largest side at index k and use two-pointer method
+        for k in range(n - 1, 1, -1):  # Iterate from the last element
+            i, j = 0, k - 1  # Two pointers at start and just before k
+            
+            while i < j:
+                if nums[i] + nums[j] > nums[k]:  # Valid triangle condition
+                    count += (j - i)  # All pairs (i, j), (i+1, j), ..., (j-1, j) form a valid triangle
+                    j -= 1  # Move j to check other possibilities
                 else:
-                    left += 1
+                    i += 1  # Increase i to get a larger sum
+        
         return count
+
+
+
+        
