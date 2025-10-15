57f905540783851f86b4a9bf3c94fe8059197214
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-10-15 17:33:07 +0530

    Time: 82 ms (66.67%) | Memory: 17.8 MB (45.16%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.py
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.py
@@ -1,15 +1,16 @@
 class Solution:
-    def hasIncreasingSubarrays(self, nums: list[int], k: int) -> bool:
-        prev, increaseCount = 0, 1
+    def hasIncreasingSubarrays(self, nums, k):
+        n = len(nums)
+        inc = [1] * n  # inc[i] = length of increasing subarray ending at i
 
-        for i in range(1, len(nums)):
-            if nums[i-1] < nums[i]:
-                increaseCount += 1
-            else:
-                prev = increaseCount
-                increaseCount = 1
+        # Step 1: build increasing lengths
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
+                inc[i] = inc[i - 1] + 1
 
-            if increaseCount // 2 >= k or prev // 2 >= k or min(prev, increaseCount) >= k:
+        # Step 2: check two adjacent increasing subarrays of size k
+        for i in range(k - 1, n - k):
+            if inc[i] >= k and inc[i + k] >= k:
                 return True
 
         return False
