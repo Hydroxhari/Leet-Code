5d9acb4df33e64bcb07df7c514a3e5f418bef8eb
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-10-17 18:33:17 +0530

    Time: 7 ms (59.61%) | Memory: 13.7 MB (37.05%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,7 +1,8 @@
 class Solution(object):
-    def moveZeroes(self, nums):
-        pos = 0  # Position to place next non-zero
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[pos], nums[i] = nums[i], nums[pos]
-                pos += 1
+    def moveZeroes(self, n):
+        p=0
+        for i in range(len(n)):
+            if n[i]!=0:
+                n[p],n[i]=n[i],n[p]
+                p+=1
+        return n
