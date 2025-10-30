24290085486d6d527d22ed1791ce4525ddf2b702
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-10-30 12:31:20 +0530

    Time: 15 ms (99.53%) | Memory: 20 MB (93.88%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,15 +1,23 @@
 class Solution(object):
     def productExceptSelf(self, n):
+        s=1
+        c=0
+        for i in n:
+            if i==0:
+                c+=1
+            else:
+                s*=i
+            
+        l=[0]*len(n)
+        if c>1:
+            return l
         
-        l=[1]
-        p=1
-        for i in n[:-1]:
-            p=p*i
-            l.append(p)
-        p=1
-        print(l)
-        for i in range(len(n)-1,-1,-1):
-            l[i]*=p
-            p=p*n[i]
+        elif c==1:
+            for i in range(len(n)):
+                if n[i]==0:
+                    l[i]=s
+        else:
+            for i in range(len(n)):
+                l[i]=s//n[i]
         return l
-
+        
