5ffdd900d7f8c538e9b1fff66d63d0d1ab9bde13
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-11-04 11:59:55 +0530

    Time: 7 ms (96.21%) | Memory: 17 MB (23.80%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,17 +1,13 @@
 class Solution(object):
-    def merge(self, l):
-        l.sort()
+    def merge(self, n):
+        n.sort(key = lambda x:x[0])
 
-        fs,fe=l[0][0],l[0][1]
-        s=[l[0]]
-        for i,j in l[1:]:
-            if i<=fe<=j:
-                fe=max(fe,j)
-                s[-1][1]=fe
-            elif i>fe:
-                s.append([i,j])
-                fs,fe=i,j
-
+        s=[n[0]]
+        for i in n[1:]:
+            a,b=i
+            if a<=s[-1][1]:
+                s[-1][1]=max(s[-1][1],b)
+            else:
+                s.append([a,b])
         return s
-
-
+            
