af8e63a33460b804311451b0949922243c56959a
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-11-04 23:58:05 +0530

    Time: 43 ms (43.49%) | Memory: 12.7 MB (81.46%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,21 +1,23 @@
+from collections import Counter
+
 class Solution(object):
     def checkInclusion(self, s1, s2):
-
-        a=len(s1)
-        b=len(s2)
-        if a>b:
+        a, b = len(s1), len(s2)
+        if a > b:
             return False
-        c=Counter(s1)
-        l=Counter(s2[:a])
 
-        def dp(l):
-            return all(l[i]==j for i,j in c.items())
+        c = Counter(s1)
+        l = Counter(s2[:a])
 
-        if dp(l):
+        if c == l:
             return True
-        for i in range(a,b):
-            l[s2[i-a]]-=1
-            l[s2[i]]+=1
-            if dp(l):
+
+        for i in range(a, b):
+            l[s2[i]] += 1
+            l[s2[i - a]] -= 1
+            if l[s2[i - a]] == 0:
+                del l[s2[i - a]]
+            if l == c:
                 return True
+
         return False
