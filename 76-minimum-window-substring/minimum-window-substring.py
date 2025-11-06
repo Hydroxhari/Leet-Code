71527663da36b81f417cd4d7222215f18f9c4398
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-11-06 11:42:58 +0530

    Time: 1418 ms (5.02%) | Memory: 15.8 MB (11.91%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,20 +1,19 @@
-from collections import Counter
-
 class Solution(object):
     def minWindow(self, s, t):
-        r = Counter(t)
-        l = Counter()
-        res = ''
-        left = 0
 
-        for right in range(len(s)):
-            l[s[right]] += 1
-
-            # shrink from left while all conditions are satisfied
-            while all(l[ch] >= r[ch] for ch in r):
-                if not res or len(res) > right - left + 1:
-                    res = s[left:right+1]
-                l[s[left]] -= 1
-                left += 1
-
-        return res
+        a,b=len(s),len(t)
+        if b>a:
+            return ''
+        
+        c=Counter(t)
+        r=Counter()
+        m=s*2
+        j=0
+        for i in range(a):
+            r[s[i]]+=1
+            while all(r[x]>=y for x,y in c.items()):
+                if len(m)>i-j+1:
+                    m=s[j:i+1]
+                r[s[j]]-=1
+                j+=1
+        return '' if m==s*2 else m
