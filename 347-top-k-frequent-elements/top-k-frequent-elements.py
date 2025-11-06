82b502088090f1c12c058b35689284a26ab15b01
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-11-06 12:58:17 +0530

    Time: 12 ms (45.32%) | Memory: 14 MB (99.79%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,23 +1,7 @@
+import heapq
+from collections import Counter
+
 class Solution(object):
     def topKFrequent(self, n, k):
-
-        b=[[] for i in range(len(n)+1)]
-
-        h={}
-        for i in n:
-            if i not in h:
-                h[i]=0
-            h[i]+=1
-        
-        for i,j in h.items():
-            b[j].append(i)
-        
-        c=0
-        l=[]
-        for i in range(len(n),0,-1):
-            for j in b[i]:
-                l.append(j)
-                c+=1
-                if c==k:
-                    return l
-        return -1
+        c = Counter(n)
+        return heapq.nlargest(k, c.keys(), key=lambda x: c[x])
