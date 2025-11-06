5fbc32d6d4a9b2a7af4e3f5ab6f3a54b34fd559b
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-11-06 14:51:55 +0530

    Time: 393 ms (16.05%) | Memory: 13.3 MB (77.62%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,21 +1,16 @@
 class Solution(object):
     def pivotIndex(self, n):
 
-        p=[0]*len(n)
-        for i in range(len(n)-1):
-            p[i+1]=p[i]+n[i]
+        r=sum(n)
+        l=0
         
-        s=0
-        pi=-1
-        for j in range(len(n)-1,-1,-1):
-            if s==p[j]:
-                pi=j
-            s+=n[j]
-        
-        return pi
+        for i in range(len(n)):
+            r-=n[i]
+            print(l,r)
+            if l==r:
+                return i
+            l+=n[i]
+            
+        return -1
 
-        [ 0,1,8,11,17,22]
-        [27,20,17,11,6,0]
 
-        [0,2,3]
-        [0,-1,0]
