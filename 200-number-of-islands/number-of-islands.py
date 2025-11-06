5586ecfcf0cfe02146dbd672d101d60629fc243c
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-11-06 19:47:30 +0530

    Time: 210 ms (90.56%) | Memory: 27.6 MB (87.98%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,33 +1,26 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+from collections import deque
 
-
-class Solution(object):
+class Solution:
     def numIslands(self, grid):
         if not grid:
             return 0
         
-        rows, cols = len(grid), len(grid[0])
-        visited = set()
-        directions = [(-1,0), (1,0), (0,-1), (0,1)]
+        m, n = len(grid), len(grid[0])
         count = 0
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
         
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == '1' and (i,j) not in visited:
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
                     count += 1
-                    queue = deque([(i,j)])
-                    visited.add((i,j))
+                    q = deque([(i,j)])
+                    grid[i][j] = '0'
                     
-                    while queue:
-                        r, c = queue.popleft()
-                        for dr, dc in directions:
-                            nr, nc = r + dr, c + dc
-                            if (0 <= nr < rows and 0 <= nc < cols and 
-                                grid[nr][nc] == '1' and (nr, nc) not in visited):
-                                queue.append((nr, nc))
-                                visited.add((nr, nc))
-        
+                    while q:
+                        x, y = q.popleft()
+                        for dx, dy in directions:
+                            nx, ny = x + dx, y + dy
+                            if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == '1':
+                                grid[nx][ny] = '0'
+                                q.append((nx, ny))
         return count
-
-
-
