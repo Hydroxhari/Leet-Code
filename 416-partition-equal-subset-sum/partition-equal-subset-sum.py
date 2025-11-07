6d50eeeda0df1c1677822f22a71d98a912eaa13b
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-11-07 18:04:49 +0530

    Time: 3515 ms (12.74%) | Memory: 286 MB (5.03%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -2,15 +2,22 @@ class Solution(object):
     def canPartition(self, n):
 
         s=sum(n)
+        m=s//2
         if s%2!=0:
             return False
+        
+        d=defaultdict(bool)
+        def dp(i,r):
+            if (i,r) in d:
+                return d[(i,r)]
+            if r==0:
+                return True
+            if i>=len(n):
+                return False
             
-        r=s//2
+            d[(i,r)] = dp(i+1,r-n[i]) or dp(i+1,r)
+            return d[(i,r)]
+        
+        return dp(0,m)
 
-        dp=[1]+[0]*(r)
-
-        for i in n:
-            for j in range(r,i-1,-1):
-                dp[j]=dp[j] or dp[j-i]
-                
-        return dp[-1]==1
+        
