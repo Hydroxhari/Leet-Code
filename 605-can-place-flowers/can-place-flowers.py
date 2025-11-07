a4b469c5cd5e24a9d4228904ca83992254dc7228
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-11-07 23:02:48 +0530

    Time: 10 ms (62.06%) | Memory: 13.2 MB (39.57%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,26 +1,11 @@
 class Solution(object):
-    def canPlaceFlowers(self, f, n):
+    def canPlaceFlowers(self, flowerbed, n):
+        f = [0] + flowerbed + [0]
 
-        if n==0:
-            return True
-        dp=[0]+f+[0]
-        k=n
-
-        for i in range(1,len(f)+1):
-            print(dp[i])
-            if dp[i]==0 and dp[i-1]==0 and dp[i+1]==0:
-                dp[i]=1
-                n-=1
-            if n==0:
-                return True
-        
-        dp=[0]+f[::-1]+[0]
-        n=k
-
-        for i in range(1,len(f)+1):
-            if dp[i]==0 and dp[i-1]==0 and dp[i+1]==0:
-                dp[i]=1
-                n-=1
-            if n==0:
-                return True
-        return False
+        for i in range(1, len(f) - 1):
+            if f[i - 1] == f[i] == f[i + 1] == 0:
+                f[i] = 1
+                n -= 1
+                if n == 0:
+                    return True
+        return n <= 0
