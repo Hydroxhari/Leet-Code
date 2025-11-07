6e57546138a17d2bd123b5a21ed10f43ff78b671
Hydroxhari <86720942+Hydroxhari@users.noreply.github.com>
2025-11-07 19:01:03 +0530

    Time: 227 ms (61.15%) | Memory: 27.4 MB (97.28%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,26 +1,20 @@
-from collections import deque
+class Solution(object):
+    def numIslands(self, g):
+        
+        m,n=len(g),len(g[0])
+        c=0
 
-class Solution:
-    def numIslands(self, grid):
-        if not grid:
-            return 0
-        
-        m, n = len(grid), len(grid[0])
-        count = 0
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
-        
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1':
-                    count += 1
-                    q = deque([(i,j)])
-                    grid[i][j] = '0'
-                    
+                if g[i][j]=='1':
+                    c+=1
+                    q=deque([(i,j)])
+                    g[i][j]='0'
                     while q:
-                        x, y = q.popleft()
-                        for dx, dy in directions:
-                            nx, ny = x + dx, y + dy
-                            if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == '1':
-                                grid[nx][ny] = '0'
-                                q.append((nx, ny))
-        return count
+                        a,b = q.popleft()
+                        for x,y in [(1,0),(0,1),(-1,0),(0,-1)]:
+                            na,nb=x+a,y+b
+                            if 0<=na<m and 0<=nb<n and g[na][nb]=='1':
+                                g[na][nb]='0'
+                                q.append((na,nb))
+        return c
